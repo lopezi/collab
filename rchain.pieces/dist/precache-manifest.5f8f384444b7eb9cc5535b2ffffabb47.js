5ab3fefbbd1566623f8c6d654fe60571cabb4ef7
Nuno Lopes <nuno.lopes@lopezi.com>
2020-02-20 14:14:13 +0000

    Publish /dist to enable Github pages

---
+++ rchain.pieces/dist/precache-manifest.5f8f384444b7eb9cc5535b2ffffabb47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e86b25e18c90a4e27e29",
+    "url": "/js/chunk-vendors.130e5b99.js"
+  },
+  {
+    "revision": "7bf4a8ba9ea54fc499a5",
+    "url": "/js/app.efd3ee58.js"
+  },
+  {
+    "revision": "25d6a47de1defcece610fd2875b77b31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e2352d3eea69bdebbce418bf2c50671",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "e86b25e18c90a4e27e29",
+    "url": "/css/chunk-vendors.755a9159.css"
+  },
+  {
+    "revision": "7bf4a8ba9ea54fc499a5",
+    "url": "/css/app.4302c90a.css"
+  },
+  {
+    "revision": "5f52dbefcf63c9f677ace09fcf8bc7d8",
+    "url": "/_redirects"
+  }
+];
